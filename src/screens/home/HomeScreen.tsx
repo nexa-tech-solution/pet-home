383272ac85800677668048403f5b75341710b7a2
Tien25012003 <nguyenthiphuongtien12e@gmail.com>
2026-01-05 21:32:00 +0700

    Feat | Add Admob

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,14 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
 
 import { WebView } from 'react-native-webview';
+import {
+  BannerAd,
+  BannerAdSize,
+  TestIds,
+} from 'react-native-google-mobile-ads';
+
+const adUnitId = __DEV__
+  ? TestIds.ADAPTIVE_BANNER
+  : 'ca-app-pub-9163464406132797/1322315715';
 
 const HomeScreen = () => {
   return (
-    <WebView
-      source={{ uri: 'https://pet-simulator-sigma.vercel.app/' }}
-      style={styles.container}
-    />
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: 'https://pet-simulator-sigma.vercel.app/' }}
+        //style={styles.container}
+      />
+      <BannerAd
+        unitId={adUnitId}
+        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+      />
+    </View>
   );
 };
 
